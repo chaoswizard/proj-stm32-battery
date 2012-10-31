c57c471e6fb739fec68114040b29af9586de33d6
helloworld <helloworld@7c7e67b1-f07c-664a-937c-a3f93c50a6f4>
2012-10-31 11:24:40 +0000

    git-svn-id: https://xpm_usr:8443/svn/arm_proj@43 7c7e67b1-f07c-664a-937c-a3f93c50a6f4

--- trunk/battery/source/USER/input_output.h
+++
@@ -1,9 +0,0 @@
-#ifndef __INPUT_OUTPUT_H
-#define __INPUT_OUTPUT_H
-void Write595(uint8_t qdata);
-void flashIO(void);
-void Write4094E(unsigned char dat);
-unsigned long input_165(void);
-void changeChannel(uint32_t ch);
-void Bublesort(uint16_t a[],int n);
-#endif
